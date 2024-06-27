ceeb1afd9bdec19e36effb4d1c54eddcf248be79
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-06-27 22:28:22 +0530

    ham fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,22 +15,22 @@ const Header = ({ toggleTheme, toggleMenu, showMenu }) => {
         <ul
           className={`flex-col md:flex-row md:flex md:items-center transition-all duration-300 ease-in-out ${
             showMenu ? "flex" : "hidden"
-          } absolute top-16 left-0 right-0 bg-white md:bg-transparent p-5 md:p-0 md:relative md:top-0 md:left-0 md:right-auto md:w-auto md:h-auto w-full h-screen items-center justify-center`}
+          } absolute top-16 left-0 right-0 bg-white md:bg-transparent p-5 md:p-0 md:relative md:top-0 md:left-0 md:right-auto md:w-auto md:h-auto w-full h-screen items-end`}
         >
-          <button className="px-2 py-1 text-xl" onClick={toggleTheme}>
+          <button className="px-2 py-1 text-2xl" onClick={toggleTheme}>
             <CgDarkMode />
           </button>
-          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline">
+          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline text-2xl md:text-base">
             <Link to="/blog" onClick={toggleMenu}>
               blog
             </Link>
           </li>
-          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline">
+          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline text-2xl md:text-base">
             <Link to="/wallpaper" onClick={toggleMenu}>
               wallpaper
             </Link>
           </li>
-          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline">
+          <li className="px-2 py-1 font-medium transition duration-300 ease-in-out hover:underline text-2xl md:text-base">
             <Link to="/project" onClick={toggleMenu}>
               project
             </Link>
