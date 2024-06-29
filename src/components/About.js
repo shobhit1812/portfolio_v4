993295d3fc7bd848254fee6c4d675723a2dd3e07
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-06-29 13:52:35 +0530

    changes some margin & padding

--- src/components/About.js
+++ src/components/About.js
@@ -4,29 +4,37 @@ const About = () => {
     <>
       <div className="p-5 font-poppins flex justify-between items-center mx-auto max-w-screen-xl px-6 md:px-16 lg:px-36">
         <div>
-          <h1 className="text-8xl lg:text-8xl">Hi.</h1>
-          <h1>
-            <span className="text-8xl lg:text-8xl">I'm</span>{" "}
-          </h1>
-          <h1>
-            <span className="text-8xl lg:text-8xl">
-              {name.split("").map((char, index) => (
-                <span
-                  key={index}
-                  className={`font-extrabold inline-block opacity-0 animate-typewriter`}
-                  style={{ animationDelay: `${index * 0.1}s` }}
-                >
-                  <span className="text-7xl lg:text-8xl">
-                    {char === " " ? "\u00A0" : char}
+          <div className="py-2">
+            <h1 className="text-8xl lg:text-8xl">Hi.</h1>
+          </div>
+          <div className="py-2">
+            <h1>
+              <span className="text-8xl lg:text-8xl">I'm</span>{" "}
+            </h1>
+          </div>
+          <div className="py-2">
+            <h1>
+              <span className="text-8xl lg:text-8xl">
+                {name.split("").map((char, index) => (
+                  <span
+                    key={index}
+                    className={`font-extrabold inline-block opacity-0 animate-typewriter`}
+                    style={{ animationDelay: `${index * 0.1}s` }}
+                  >
+                    <span className="text-7xl lg:text-8xl">
+                      {char === " " ? "\u00A0" : char}
+                    </span>
                   </span>
-                </span>
-              ))}
-            </span>
-          </h1>
-          <h1 className="m-1 p-1 text-5xl">
-            <span className="underline">coding instructor</span> &{" "}
-            <span className="underline">mentor</span>
-          </h1>
+                ))}
+              </span>
+            </h1>
+          </div>
+          <div className="py-6">
+            <h1 className="m-1 p-1 text-5xl">
+              <span className="underline">coding instructor</span> &{" "}
+              <span className="underline">mentor</span>
+            </h1>
+          </div>
         </div>
       </div>
       <div className="p-5 font-poppins flex justify-between items-center mx-auto max-w-screen-xl px-6 md:px-16 lg:px-36">
