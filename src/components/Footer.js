699b6cced7dde8f86e4200d4f3c78adb210b8fa8
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-06-29 13:38:03 +0530

    fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,53 +9,59 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className=" py-5 font-poppins flex justify-between items-center mx-auto max-w-screen-xl px-6 md:px-16 lg:px-36">
-      <h1 className="font-extrabold text-lg">
-        Shobhit Nautiyal &copy; {currentYear}
-      </h1>
-      <div className="flex space-x-4">
-        <a
-          href="https://github.com/Shobhit1812"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FiGithub />
-        </a>
-        <a
-          href="mailto:shobhit.nautiyal99@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SiGmail />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/shobhit-nautiyal-033b711ba/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedinIn />
-        </a>
-        <a
-          href="https://x.com/shobhitnautiya_"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaXTwitter />
-        </a>
-        <a
-          href="https://instagram.com/imshobhitnautiyal"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaInstagram />
-        </a>
-        <a
-          href="https://medium.com/@shobhit.nautiyal99"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaMedium />
-        </a>
+    <footer className="shadow-inner p-5 font-poppins">
+      <div className="flex justify-between items-center mx-auto max-w-screen-xl px-6 md:px-16 lg:px-36">
+        <h1 className="">Shobhit Nautiyal &copy; {currentYear}</h1>
+        <div className="flex">
+          <a
+            href="https://github.com/Shobhit1812"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <FiGithub />
+          </a>
+          <a
+            href="mailto:shobhit.nautiyal99@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <SiGmail />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/shobhit-nautiyal-033b711ba/"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://x.com/shobhitnautiya_"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href="https://instagram.com/imshobhitnautiyal"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://medium.com/@shobhit.nautiyal99"
+            target="_blank"
+            rel="noreferrer"
+            className="px-2 py-1"
+          >
+            <FaMedium />
+          </a>
+        </div>
       </div>
     </footer>
   );
